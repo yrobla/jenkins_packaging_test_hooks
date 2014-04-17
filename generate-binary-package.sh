29d72c7367ad53c282c97c97d3d0cbf29cfb1cff
Yolanda Robla <yolanda.robla-mota@hp.com>
2014-04-17 14:30:05 +0000

    updated to generate binaries

---
+++ generate-binary-package.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Purpose: prepare setup and invoke Debian binary package build
+
+# configuration for build-and-provide-package and its tools
+export DEB_BUILD_OPTIONS="parallel=$(nproc)"
+export BUILD_ONLY=true
+export SUDO_CMD=sudo
+export COWBUILDER_DIST=saucy
+export POST_BUILD_HOOK=/home/ubuntu/jenkins_packaging_test/generate-debc.sh
+
+# execute main jenkins-debian-glue script
+/usr/bin/build-and-provide-package
