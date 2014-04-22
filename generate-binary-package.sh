fa92aa08afa56444bc13bc96541adc3d39a03b65
Yolanda Robla <yolanda.robla-mota@hp.com>
2014-04-22 14:37:47 +0000

    fixing builders

--- generate-binary-package.sh
+++ generate-binary-package.sh
@@ -6,6 +6,7 @@ export DEB_BUILD_OPTIONS="parallel=$(nproc)"
 export BUILD_ONLY=true
 export SUDO_CMD=sudo
 export COWBUILDER_DIST=saucy
+export distribution=saucy
 export POST_BUILD_HOOK=/home/ubuntu/jenkins_packaging_test/generate-debc.sh
 
 # execute main jenkins-debian-glue script
