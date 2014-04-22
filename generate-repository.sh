abd4cd4b414d612d05b695e3a6faea295709e6f0
Yolanda Robla <yolanda.robla-mota@hp.com>
2014-04-22 08:23:27 +0000

    unset skip_removal add lintian checks

--- generate-repository.sh
+++ generate-repository.sh
@@ -4,7 +4,6 @@ export BASE_PATH="binaries/"
 export PROVIDE_ONLY=true
 export REPOSITORY="/srv/repository"
 export REPOS="saucy"
-export SKIP_REMOVAL=true
 
 /usr/bin/generate-reprepro-codename "${REPOS}"
 /usr/bin/build-and-provide-package
