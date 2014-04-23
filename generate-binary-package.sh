08ec9b88766f2f0cb519a3efd3ed7a304c0741c2
Yolanda Robla <yolanda.robla-mota@hp.com>
2014-04-23 09:29:05 +0000

    generating for debian/unstable

--- generate-binary-package.sh
+++ generate-binary-package.sh
@@ -5,8 +5,8 @@
 export DEB_BUILD_OPTIONS="parallel=$(nproc)"
 export BUILD_ONLY=true
 export SUDO_CMD=sudo
-export COWBUILDER_DIST=saucy
-export distribution=saucy
+export COWBUILDER_DIST=unstable
+export distribution=unstable
 export POST_BUILD_HOOK=/home/ubuntu/jenkins_packaging_test/generate-debc.sh
 
 # execute main jenkins-debian-glue script
--- generate-repository.sh
+++ generate-repository.sh
@@ -3,7 +3,7 @@
 export BASE_PATH="binaries/"
 export PROVIDE_ONLY=true
 export REPOSITORY="/srv/repository"
-export REPOS="saucy"
+export REPOS="unstable"
 
 /usr/bin/generate-reprepro-codename "${REPOS}"
 /usr/bin/build-and-provide-package
